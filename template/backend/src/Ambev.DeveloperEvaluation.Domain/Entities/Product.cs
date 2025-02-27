36d3408990e38d37efce366b5028b5c95e2cfec5
AugustinhoCelestino <58995280+AugustinhoCelestino@users.noreply.github.com>
2025-02-27 15:17:42 -0300

    add ProductConfigurarion mapping

--- template/backend/src/Ambev.DeveloperEvaluation.Domain/Entities/Product.cs
+++ template/backend/src/Ambev.DeveloperEvaluation.Domain/Entities/Product.cs
@@ -4,8 +4,9 @@ namespace Ambev.DeveloperEvaluation.Domain.Entities
 {
     public class Product : BaseEntity
     {
+        public string? Code { get; set; }
         public string? Description { get; set; }
-        public int UnitPrice { get; set; }
+        public decimal UnitPrice { get; set; }
 
     }
 }
