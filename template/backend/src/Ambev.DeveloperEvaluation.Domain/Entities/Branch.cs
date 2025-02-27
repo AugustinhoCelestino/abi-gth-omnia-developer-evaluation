294c9c79f8886f3e296cff6aa30f2acffb8d3c43
AugustinhoCelestino <58995280+AugustinhoCelestino@users.noreply.github.com>
2025-02-27 14:50:43 -0300

    add Branch entity

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Domain/Entities/Branch.cs
@@ -0,0 +1,10 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Common;
+
+namespace Ambev.DeveloperEvaluation.Domain.Entities
+{
+    public class Branch : BaseEntity
+    {
+        public string? Code { get; set; }
+        public string? Description { get; set; }
+    }
+}
