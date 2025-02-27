303ac452d4e4029702c5d49ddd41775ced3c0ceb
AugustinhoCelestino <58995280+AugustinhoCelestino@users.noreply.github.com>
2025-02-27 15:20:32 -0300

    add SaleItemConfiguration mapping

--- template/backend/src/Ambev.DeveloperEvaluation.Domain/Entities/SaleItem.cs
+++ template/backend/src/Ambev.DeveloperEvaluation.Domain/Entities/SaleItem.cs
@@ -4,9 +4,9 @@ namespace Ambev.DeveloperEvaluation.Domain.Entities
 {
     public class SaleItem : BaseEntity
     {
-        public Guid ProductId { get; set; }
         public Guid SaleId { get; set; }
         public Sale? Sale { get; set; }
+        public Guid ProductId { get; set; }
         public Product? Product { get; set; }
         public decimal Quantity { get; set; }
         public decimal Discount { get; set; }
---
+++ template/backend/src/Ambev.DeveloperEvaluation.ORM/Mapping/SaleConfiguration - Cópia.cs
@@ -0,0 +1,24 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Text.RegularExpressions;
+
+namespace Ambev.DeveloperEvaluation.ORM.Mapping;
+
+public class SaleItemConfiguration : IEntityTypeConfiguration<SaleItem>
+{
+    public void Configure(EntityTypeBuilder<SaleItem> builder)
+    {
+        builder.ToTable("SaleItems");
+
+        builder.HasKey(u => u.Id);
+        builder.Property(u => u.Id).HasColumnType("uuid").HasDefaultValueSql("gen_random_uuid()");
+
+        builder.Property(u => u.SaleId).IsRequired().HasColumnType("uuid");
+        builder.Property(u => u.ProductId).IsRequired().HasColumnType("uuid");
+        builder.Property(u => u.Quantity).IsRequired().HasColumnType("Decimal(18,2)");
+        builder.Property(u => u.Discount).IsRequired().HasColumnType("Decimal(18,2)");
+        builder.Property(u => u.TotalAmount).IsRequired().HasColumnType("Decimal(18,2)");
+
+    }
+}
