a09a8a435046ad2d84543ad800ba287baa889e71
AugustinhoCelestino <58995280+AugustinhoCelestino@users.noreply.github.com>
2025-02-27 22:46:03 -0300

    add IRepository Interface and Repository

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Domain/Repositories/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ambev.DeveloperEvaluation.Domain.Repositories
+{
+    public interface IRepository<TEntity> : IDisposable where TEntity : class
+    {
+        Task<TEntity> CreateAsync(TEntity model, CancellationToken cancellationToken = default);
+        Task<TEntity> DeleteAsync(TEntity model, CancellationToken cancellationToken = default);
+        Task<TEntity?> FindNoTrackingAsync(System.Linq.Expressions.Expression<Func<TEntity, bool>> where);
+        Task<IEnumerable<TEntity>> GetAllAsync(System.Linq.Expressions.Expression<Func<TEntity, bool>> where, CancellationToken cancellationToken = default);
+        Task<IEnumerable<TEntity>> Select(System.Linq.Expressions.Expression<Func<TEntity, bool>> where, Func<IQueryable<TEntity>, IQueryable<TEntity>> includes);
+        Task<IEnumerable<TEntity>> SelectMany(List<System.Linq.Expressions.Expression<Func<TEntity, bool>>> where, Func<IQueryable<TEntity>, IQueryable<TEntity>> includes);
+        Task<TEntity> UpdateAsync(TEntity model, CancellationToken cancellationToken = default);
+    }
+}
---
+++ template/backend/src/Ambev.DeveloperEvaluation.ORM/Repositories/Repository.cs
@@ -0,0 +1,95 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Common;
+using Ambev.DeveloperEvaluation.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Ambev.DeveloperEvaluation.ORM.Repositories
+{
+    public abstract class Repository<TEntity, TContext> : IRepository<TEntity>  where TEntity : BaseEntity where TContext : DbContext
+    {
+        protected readonly TContext _context;
+        protected Repository(TContext context)
+        {
+            _context = context;
+        }
+
+        public DbSet<TEntity> DbSet => _context.Set<TEntity>();
+
+        public async Task<TEntity> CreateAsync(TEntity model, CancellationToken cancellationToken = default)
+        {
+            await DbSet.AddAsync(model, cancellationToken);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return model;
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>> where, CancellationToken cancellationToken = default)
+        {
+            var result = await DbSet.AsQueryable().AsNoTracking().Where(where).ToListAsync();
+
+            return result;
+        }
+
+        public async Task<TEntity> UpdateAsync(TEntity model, CancellationToken cancellationToken = default)
+        {
+            _context.Entry(model).State = EntityState.Modified;
+            _context.Update(model);
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return model;
+        }
+
+        public async Task<TEntity> DeleteAsync(TEntity model, CancellationToken cancellationToken = default)
+        {
+            _context.Remove(model);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return model;
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<TEntity>> Select(Expression<Func<TEntity, bool>> where, Func<IQueryable<TEntity>, IQueryable<TEntity>> includes)
+        {
+            var result = DbSet.AsQueryable().AsNoTracking();
+
+            result = result.Where(where);
+
+            if (includes.Count > 0)
+            {
+                foreach (var include in includes)
+                {
+                    result = include(result);
+                }
+            }
+            return await result.ToListAsync();
+        }
+
+        public async Task<IEnumerable<TEntity>> SelectMany(List<Expression<Func<TEntity, bool>>> where, Func<IQueryable<TEntity>, IQueryable<TEntity>> includes)
+        {
+            var result = DbSet.AsQueryable().AsNoTracking();
+
+            foreach (var filter in where)
+            {
+                result = result.Where(filter);
+            }
+
+            if (includes != null)
+            {
+                result = includes(result);
+            }
+
+            return await result.ToListAsync();
+
+        }
+
+        public async Task<TEntity?> FindNoTrackingAsync(Expression<Func<TEntity, bool>> where)
+        {
+            return await DbSet.AsNoTracking().FirstOrDefaultAsync(where);
+        } 
+    }
+}
